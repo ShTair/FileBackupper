75bb8bc7a67a4ca1ddb7137721c8e60800c919bc
ShTair <git@vsh.jp>
2017-10-14 08:14:31 +0900

    リストアを実装

--- FileRestorer/Program.cs
+++ FileRestorer/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using FileBackupper.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace FileRestorer
 {
@@ -10,6 +10,28 @@ namespace FileRestorer
     {
         static void Main(string[] args)
         {
+            var json = File.ReadAllText(args[0]);
+            var snaps = JsonConvert.DeserializeObject<List<Snapshot>>(json);
+
+            var srcDir = Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(args[0])), "Data");
+            var dir = args[1];
+
+            foreach (var snap in snaps)
+            {
+                foreach (var path in snap.Paths)
+                {
+                    var fullName = Path.Combine(dir, path.Path);
+                    Directory.CreateDirectory(Path.GetDirectoryName(fullName));
+
+                    var srcName = Path.Combine(srcDir, ((path.Id ?? 0) % 100).ToString("00"), ((path.Id ?? 0) % 10000).ToString("0000"), path.Id.ToString());
+
+                    File.Copy(srcName, fullName);
+                    File.SetCreationTime(fullName, DateTime.FromBinary(path.Creation));
+                    File.SetLastWriteTime(fullName, DateTime.FromBinary(path.LastWrite));
+
+                    Console.WriteLine(path.Path);
+                }
+            }
         }
     }
 }
